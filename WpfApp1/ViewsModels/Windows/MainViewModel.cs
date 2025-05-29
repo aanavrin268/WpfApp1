cbe0a673c9a962c5f2a3cb074256554f00865efb
aanavrin268 <aanavrin268@gmail.com>
2025-05-29 09:44:25 -0600

    mvvm

---
+++ WpfApp1/ViewsModels/Windows/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewsModels.Windows
+{
+    public partial class MainViewModel: ObservableObject
+    {
+
+        [ObservableProperty]
+        private string _message = "Holaa, desde VM";
+    }
+}
